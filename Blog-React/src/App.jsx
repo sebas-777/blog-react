f181850fbda46f7c45f550a6f8e526f9103c0cd5
Sebastian Castro <scastro@qvision.com.co>
2024-02-07 19:55:23 -0500

    implementacion de la logica en App.jsx

--- Blog-React/src/App.jsx
+++ Blog-React/src/App.jsx
@@ -1,13 +1,45 @@
 
 import './App.css';
 import Testimony from './components/Testimony';
+import emma from '../src/assets/emma.jpeg';
+import shawn from '../src/assets/shawn.jpeg'
+import sarah from '../src/assets/sarah.jpeg'
 
 function App() {
  
 
-  return (
-   
-     <Testimony />
+  return ( 
+
+   <div className='App'> 
+    <div className='container-main'>
+      <h1>Here is what our alumni say about freeCodeCamp:</h1>
+     <Testimony 
+     name ='Shawn Wang '
+     country = 'Singapore'
+     image={shawn}
+     job = 'Software Enginee'
+     company = 'Amazon'
+     testimony = "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life. "
+     />
+     <Testimony 
+     name ='Sarah Chima'
+     country = 'Nigeria'
+     image={sarah}
+     job = 'Software Enginee'
+     company = 'ChatDesk'
+     testimony = "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+     />
+      <Testimony   
+     name ='Emma Bostian'
+     country = 'Sweden'
+     image={emma}
+     job = 'Software Enginee'
+     company = 'Spotify'
+     testimony = "'I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.' "
+     />
+    </div>
+   </div>
+    
     
   )
 }
--- Blog-React/src/components/Testimony.jsx
+++ Blog-React/src/components/Testimony.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import emma from '../assets/emma.jpeg'
 
-function Testimony() {
-return(
+import React from 'react';
+import '../styleSheets/Testimony.css'
+
+
+function Testimony(props) { 
+
+return( 
 		<div className='container-testimony'>
 			<img 
 			className='image-testimony'
-			src={emma} 
+			src={props.image}
 			/>
 			<div className='container-text-name'>
 				<p className='container-employeName'>
-				Emma Bostian in Sweden
+				<strong>{props.name} in {props.country}</strong>
 				</p>
 				<p className='container-job'>
-				Software Engineer at Spotify
+				{props.job} at <strong>{props.company}</strong> 
 				</p>
 				<p className='container-history'>
-				"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+				{props.testimony}
 				</p>
 
 			</div>
